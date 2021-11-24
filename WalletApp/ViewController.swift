427a5bf84a65cb94496d8b4db0523b51be99d8cd
SHiN201007 <shin1007m@gmail.com>
2021-11-25 08:27:20 +0900

    first commit

---
+++ WalletApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalletApp
+//
+//  Created by 松丸真 on 2021/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
