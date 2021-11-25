2d0febd5b7161aafb55ba52567250146306ed044
SHiN201007 <shin1007m@gmail.com>
2021-11-25 09:43:10 +0900

    rootVC設定

---
+++ WalletApp/UserInterface/Wallet/WalletViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WalletViewController.swift
+//  WalletApp
+//
+//  Created by 松丸真 on 2021/11/25.
+//
+
+import UIKit
+
+class WalletViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
