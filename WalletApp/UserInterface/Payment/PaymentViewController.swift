ee1798bfba59681ab4eed80ae0444156f2e4325a
SHiN201007 <shin1007m@gmail.com>
2021-11-26 12:29:53 +0900

    UI作成

--- WalletApp/UserInterface/Payment/PaymentViewController.swift
+++ WalletApp/UserInterface/Payment/PaymentViewController.swift
@@ -11,6 +11,20 @@ import RxCocoa
 
 class PaymentViewController: UIViewController {
     
+    @IBOutlet weak var paymentView: UIView!
+    @IBOutlet weak var typeImageView: UIImageView!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var foodButton: UIButton!
+    @IBOutlet weak var lifeButton: UIButton!
+    @IBOutlet weak var entertainmentButton: UIButton!
+    @IBOutlet weak var trainButton: UIButton!
+    @IBOutlet weak var studyButton: UIButton!
+    @IBOutlet weak var otherButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+    
+    @IBOutlet var walletTypeViews: [UIView]!
+    
     private var viewModel: PaymentViewModel!
     private let disposeBag = DisposeBag()
 
@@ -18,16 +32,34 @@ class PaymentViewController: UIViewController {
         super.viewDidLoad()
         configView()
         configViewModel()
+        bind()
     }
     
     private func configView() {
         title = L10n.titlePayment
+        // paymentView
+        paymentView.do { view in
+            view.layer.cornerRadius = 10.0
+            view.configShadow()
+        }
+        // wallet type view
+        walletTypeViews.forEach { typeView in
+            typeView.layer.cornerRadius = 10.0
+            typeView.configShadow()
+        }
+        
+        doneButton.layer.cornerRadius = 10.0
+        doneButton.configGradientColor(width: self.view.bounds.width - 128, height: 60, colors: .food)
+        doneButton.configShadow()
     }
     
     private func configViewModel() {
         let input = PaymentViewModel.Input()
         viewModel = PaymentViewModel(trigger: input)
-        // MARK: OUTPUT
+    }
+    
+    private func bind() {
+        
     }
 
 }
